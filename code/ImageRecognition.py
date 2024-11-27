05eb9d6c899a321b845d19c4829bdd78edbfa042
RYUKOUKOU <liuhonghao159@gmail.com>
2024-11-27 11:28:53 +0900

    main

--- code/ImageRecognition.py
+++ code/ImageRecognition.py
@@ -9,3 +9,11 @@ def predict_init(args,x=0):
 def predict(model, img):
     res = model.predict(source=img)
     return res[0]
+
+def return_food_info(res):
+  class_ids = res.boxes.cls
+  num = len(class_ids)
+  class_names =res[0].names
+  class_id = int(class_ids[0].item())
+  class_name = class_names[class_id]
+  return class_name,num
